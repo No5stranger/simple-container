210adad9ed6135a25e0768be04d288a9dbf267d8
cjp <no5stranger@gmail.com>
2023-04-14 15:15:29 +0800

    image ref use command arg

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 
 	sc "github.com/no5stranger/simple-containerd/snapshot"
 )
 
 func main() {
-	//err := sc.BusyBoxExample()
-	err := sc.NginxExample()
+	if len(os.Args) < 2 {
+		log.Fatal("miss image ref")
+	}
+	err := sc.ContainerExample(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
--- snapshot/with_snapshot.go
+++ snapshot/with_snapshot.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/containerd/containerd/snapshots"
 	"log"
+	"strings"
 	"syscall"
 	"time"
 
@@ -14,17 +15,18 @@ import (
 	"github.com/containerd/containerd/oci"
 )
 
-func BusyBoxExample() error {
+func ContainerExample(ref string) error {
 	startTime := time.Now()
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
 		return err
 	}
 	ctx := namespaces.WithNamespace(context.Background(), "default")
-	labels := map[string]string{"containerd.io/snapshot/image-cache": "imc-busybox-latest"}
+	refName := strings.ReplaceAll(ref, ":", "-")
+	labels := map[string]string{"containerd.io/snapshot/image-cache": fmt.Sprintf("imc-%s", refName)}
 	image, err := client.Pull(
 		ctx,
-		"docker.io/library/busybox:latest",
+		fmt.Sprintf("docker.io/library/%s", ref),
 		containerd.WithPullUnpack,
 		containerd.WithPullSnapshotter("a-overlayfs", snapshots.WithLabels(labels)),
 	)
@@ -41,78 +43,9 @@ func BusyBoxExample() error {
 
 	container, err := client.NewContainer(
 		ctx,
-		"busybox-server",
+		fmt.Sprintf("%s-server", refName),
 		containerd.WithSnapshotter("a-overlayfs"),
-		containerd.WithNewSnapshot("busybox-snapshot", image),
-		containerd.WithNewSpec(oci.WithImageConfig(image)),
-	)
-	if err != nil {
-		return err
-	}
-	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
-	containerCostTime := time.Now().UnixMilli() - startTime.UnixMilli()
-	log.Printf("Containerd created, id:%s, cost:%dms", container.ID(), containerCostTime)
-
-	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
-	if err != nil {
-		return err
-	}
-	log.Printf("Task created, %s", task.ID())
-	defer task.Delete(ctx)
-	existStatus, err := task.Wait(ctx)
-	if err != nil {
-		return err
-	}
-	if err := task.Start(ctx); err != nil {
-		return err
-	}
-	log.Printf("Containerd started...")
-
-	time.Sleep(3 * time.Second)
-	if err := task.Kill(ctx, syscall.SIGKILL); err != nil {
-		return err
-	}
-	fmt.Printf("Task killed, %v", task.ID())
-
-	status := <-existStatus
-	code, _, err := status.Result()
-	if err != nil {
-		return err
-	}
-	log.Printf("Task exited, code:%d", code)
-	return nil
-}
-
-func NginxExample() error {
-	startTime := time.Now()
-	client, err := containerd.New("/run/containerd/containerd.sock")
-	if err != nil {
-		return err
-	}
-	ctx := namespaces.WithNamespace(context.Background(), "default")
-	labels := map[string]string{"containerd.io/snapshot/image-cache": "imc-nginx-stable"}
-	image, err := client.Pull(
-		ctx,
-		"docker.io/library/nginx:stable",
-		containerd.WithPullUnpack,
-		containerd.WithPullSnapshotter("a-overlayfs", snapshots.WithLabels(labels)),
-	)
-	if err != nil {
-		return err
-	}
-	log.Printf("Pulled image, name:%s", image.Name())
-	rootfs, err := image.RootFS(ctx)
-	if err != nil {
-		return err
-	}
-	pullCostTime := time.Now().UnixMilli() - startTime.UnixMilli()
-	log.Printf("Image Pulled, Rootfs:%v, cost:%dms", rootfs, pullCostTime)
-
-	container, err := client.NewContainer(
-		ctx,
-		"nginx-server",
-		containerd.WithSnapshotter("a-overlayfs"),
-		containerd.WithNewSnapshot("nginx-snapshot", image),
+		containerd.WithNewSnapshot(fmt.Sprintf("%s-snapshot", refName), image),
 		containerd.WithNewSpec(oci.WithImageConfig(image)),
 	)
 	if err != nil {
